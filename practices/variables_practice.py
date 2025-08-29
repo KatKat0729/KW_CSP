ef87d81b69737e93a71851a057c2910ae7eda76e
KatKat0729 <kat.wightman@ucas-edu.net>
2025-08-29 14:42:34 -0600

    variables notes

---
+++ practices/variables_practice.py
@@ -0,0 +1,11 @@
+school = "UCAS"
+single_digit = 4
+how_much = 10
+print(how_much)
+print(single_digit)
+
+how_much = 777777777777
+print(how_much)
+
+name = input("What is your name?")
+print("Hello"name)
