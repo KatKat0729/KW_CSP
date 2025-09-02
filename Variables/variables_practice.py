0928c0e37eb8a74d9c476202eec1f47767247675
KatKat0729 <kat.wightman@ucas-edu.net>
2025-09-02 14:36:12 -0600

    finished assignment

---
+++ Variables/variables_practice.py
@@ -0,0 +1,42 @@
+#KW_7_Variables_Practice
+
+your_name = "kathryn"
+
+number10 = 3
+
+number1000 = 777
+
+breakfast = "smoothie"
+
+favorite_color = "red"
+
+school_name = "UCAS"
+
+year = 2025
+
+eye_color = "brown"
+
+age = 14
+
+subject = "art"
+
+print(your_name)
+
+print(number10)
+
+print(number1000)
+
+print(breakfast)
+
+print(favorite_color)
+
+print(school_name)
+
+print(year)
+
+print(eye_color)
+
+print(age)
+
+print(subject)
+
