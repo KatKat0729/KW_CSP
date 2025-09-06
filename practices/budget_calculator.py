40352b2c12d6e9dc27bb9294befab409397e5649
KatKat0729 <kat.wightman@ucas-edu.net>
2025-09-06 00:58:21 +0000

    finished expressions practice and budget calculator assignment

--- practices/budget_calculator.py
+++ practices/budget_calculator.py
@@ -1,12 +1,22 @@
 # KW 8th Budget Calculator
 
-income =
-rent =
-utilities =
-groceries =
-transportation = 
-savings =
-savings is 10% income
-spend = income - (utilities + rent + groceries + transportation + +savings)
+income = float(input("What is your monthly income? "))
+rent = float(input("What is your monthly rent? "))
+utilities = float(input("What is your monthly utilities? "))
+groceries = float(input("What is your monthly groceries? "))
+trans =float(input("What is your monthly transportation? "))
+savings = income*.1
+
+spending_money = income-rent-utilities-groceries-trans-savings
+
 percent_rent = rent/income*100
-income *0.1
+percent_utilities = utilities/income*100
+percent_groceries = groceries/income*100
+percent_trans = trans/income*100
+
+print("Your rent is $", rent, "and that is ", percent_rent, "% of your income.")
+print("Your utilities are $", utilities, " and that is ", percent_utilities, "% of your income.")
+print("Your groceries is $", groceries, " and that is ", percent_groceries, "% of your income.")
+print("Your transportation is $", trans, " and that is ", percent_trans, "% of your income.")
+print("You should save $", savings, " a month, that is 10% of your income.")
+print("You have $", spending_money, " of spending money each month!")
--- practices/expressions_practice.py
+++ practices/expressions_practice.py
@@ -1,9 +1,27 @@
 # KW 7th Expressions Practice
+answer = 7-24/8*4+6
+print(answer)
 
-print(7-24/8*4+6)
-print(18/3-7+2*5)
-print(6*4/12+72/8-9)
-print((17-6/2)+4*3)
-print(-2(1*4-2/2)+(6+2-3))
-print(-1*[(3-4*7)/5]-2*24/6)
-print(3*
+answer = 18/3-7+2*5
+print(answer)
+
+answer = 6*4/12+72/8-9
+print(answer)
+
+answer = (17-6/2)+4*3
+print(answer)
+
+answer = -2*(1*4-2/2)+(6+2-3)
+print(answer)
+
+answer = -1 * ((3-4*7)/5)-2*24/6
+print(answer)
+
+answer = ((3*(5**2))/15)-(5-2**2)
+print(answer)
+
+answer = ((1**4)*(2**2)+(3**3))-(2**5)/4
+print(answer)
+
+answer = (22/2-2*5)**2+(4-6/6)**2
+print(answer)
