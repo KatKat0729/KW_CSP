f05e4e1e67273686a38b7de6fb8a03b28c64b13e
KatKat0729 <kat.wightman@ucas-edu.net>
2025-09-08 14:41:43 -0600

    started string notes

--- practices/strings.py
+++ practices/strings.py
@@ -1,2 +1,11 @@
 #KW 7th Strings notes practice
 print("I did it!")
+
+#Examples
+first_name = input("What is your first name?\n").strip()
+last_name = input("What is your last name?\n").strip()
+
+sentence = 'The quick brown fox jumps over the lazy dog'
+full_name = first_name + " " + last_name
+
+print("Welcome to my program", full_name)
