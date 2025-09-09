92681132dad62894ed2d8b4b37f112db7bc97b8c
KatKat0729 <kat.wightman@ucas-edu.net>
2025-09-09 14:40:52 -0600

    finished string notes

--- practices/strings.py
+++ practices/strings.py
@@ -5,9 +5,19 @@ print("I did it!")
 first_name = input("What is your first name?\n").strip()
 last_name = input("What is your last name?\n").strip()
 
-sentence = 'The quick brown fox jumps over the lazy dog'
+sentence = '            The quick brown fox jumps over the lazy dog               '.strip()
+print(sentence.find("jumps"))
+print(sentence[20:25])
+
 full_name = first_name + " " + last_name
 
 print("Welcome to my program", full_name)
 
 # Sanitization and stupid proofing!
+# Getting rid of spaces, fixing capitilization is sanitization
+# Stupid proofing is building your code so your users can't break it
+
+# Debugging is fixing code
+    # Syntax Error - A mistake in your grammar
+error = "This is a bug"
+print(error)
--- practices/tempCodeRunnerFile.py
+++ practices/tempCodeRunnerFile.py
@@ -1,9 +1,2 @@
-num_1 = 12
-num_2 = 7
-print("Addition(+): ", num_1+num_2)
-print("Subtraction(-): ", num_1-num_2)
-print("Multiplication(*): ", num_1*num_2)
-print("Division(/): ", num_1/num_2)
-print("Exponents(**): ", num_1**num_2)
-print("Integer Division(//): ", num_1//num_2)
-print("Modulo(%): ", num_1%num_2)
+
+print(error)
