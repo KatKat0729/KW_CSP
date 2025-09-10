b27b3a815d9cde3fb0db32a824b9896ebd992d63
KatKat0729 <kat.wightman@ucas-edu.net>
2025-09-10 00:57:01 +0000

    finished silly sentences assignment

---
+++ practices/silly_sentences.py
@@ -0,0 +1,7 @@
+# KW 7th Silly Sentences
+# "I caught [NAME] trying to [VERB] the [NOUN], and honestly, I respect the hustle."
+name = input("Give me a name of someone you know - \n") 
+verb = input("Give me a verb - \n")
+noun = input("Give a place or thing - \n")
+
+print("I caught", name, "trying to", verb, "the", noun, ", and honestly, I respect the hustle.")
