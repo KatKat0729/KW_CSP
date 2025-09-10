a498446058f1a8a5c2d03ed61ef67bc7f0cf5a57
KatKat0729 <kat.wightman@ucas-edu.net>
2025-09-10 14:28:23 -0600

    re-did silly sentences assignment

--- practices/silly_sentences.py
+++ practices/silly_sentences.py
@@ -1,7 +1,8 @@
 # KW 7th Silly Sentences
-# "I caught [NAME] trying to [VERB] the [NOUN], and honestly, I respect the hustle."
-name = input("Give me a name of someone you know - \n") 
-verb = input("Give me a verb - \n")
-noun = input("Give a place or thing - \n")
+# Roses are [ADJECTIVE], Violets are [COLOR], Sugar is [NOUN], and so are [PLURAL NOUN]
+adjective = input("Give me an adjective - \n") 
+color = input("Give me a color - \n")
+adjective2 = input("Give another adjective - \n")
+plural_noun = input("Give me a plural noun -\n")
 
-print("I caught", name, "trying to", verb, "the", noun, ", and honestly, I respect the hustle.")
+print("Roses are", adjective, ", Violets are", color,", Sugar is", adjective2, ", and so are", plural_noun)
