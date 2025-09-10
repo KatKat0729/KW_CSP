11788f6081fe8a867fa3370f58e1708b7db7c0e8
KatKat0729 <kat.wightman@ucas-edu.net>
2025-09-10 01:08:59 +0000

    finished name decorator assignment

---
+++ practices/name_decorator.py
@@ -0,0 +1,17 @@
+# KW 7th Name Decorator
+
+name = input("Hello, I am the name decorator, please tell me your name! -\n")
+
+style1 = "~^~" +name+ "~^~"
+
+style2 = "<<<" +name+ ">>>"
+
+style3 = "---" +name+ "---"
+
+style4 = "_.^._" +name+ "_.^._"
+
+
+print(style1)
+print(style2)
+print(style3)
+print(style4)
