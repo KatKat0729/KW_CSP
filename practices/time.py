6bde646101e2e09b339da3cdf98407bcb5cf88c0
KatKat0729 <kat.wightman@ucas-edu.net>
2025-09-19 14:03:25 -0600

    did some time notes

--- practices/time.py
+++ practices/time.py
@@ -2,8 +2,8 @@
 
 time = int(input("What is the hour for you in military time?"))
 
-if time <= 12:
-    print(f"It is {time} for you, good morning! (:)")
+if time < 12:
+    print(f"It is {time} for you, good morning! (:")
 elif time >= 12 and time <= 17:
     print(f"It is {time} for you, good afternoon! (:")
 elif time >= 17:
