98dd2f42aec149d8677e6a3baff93448ed1a0742
KatKat0729 <kat.wightman@ucas-edu.net>
2025-10-03 14:42:24 -0600

    finished variables C notes

--- c_programming_2/notes/variables.c
+++ c_programming_2/notes/variables.c
@@ -2,13 +2,25 @@
 #include <stdio.h>
 
 int main(void){
-    int grade = 95; // 4 bytes
+    int grade; // 4 bytes
     float pi = 3.14; // 4 bytes
     double long_pi = 3.14159265; // 8 bytes
-    char letter_grade = 'A'; // 1 byte
+    char letter_grade; // 1 byte
     char name[] = "Vinny";
-    printf("%s did it! ", name);
-    printf("You have a %d, in the class. That is an %c", grade, letter_grade);
+
+    printf("What is your grade percentage as a whole number? ");
+    scanf("%d", &grade);
+    // user input
+    
+    printf("What is your letter grade?\n");
+    scanf(" %c", &letter_grade);
+
+    printf("You have a %d in the class. That is an %c. ", grade, letter_grade);
+    
+    //printf("%s did it!\n ", name);
+
+
+    
 
     return 0;
 }
