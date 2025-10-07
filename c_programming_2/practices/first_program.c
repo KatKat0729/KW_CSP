f1214b434ac2ad19683136c8600b660eed97ec44
KatKat0729 <kat.wightman@ucas-edu.net>
2025-10-07 02:58:18 +0000

    finish fiwst pwogwam

--- c_programming_2/practices/first_program.c
+++ c_programming_2/practices/first_program.c
@@ -1,1 +1,16 @@
 // KW 7th First Program
+
+#include <stdio.h>
+
+int main(void){
+
+    char name[50]; // Declaring the variable
+
+    printf("What is your name?\n"); // Question for user
+
+    scanf("%s", name); // Ask user for input, stores that input in 'name'
+
+    printf("Hello %s! Welcome to your first C program.\n", name); //Printing the input
+
+    return 0;
+}
