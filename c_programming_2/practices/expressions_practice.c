ca3655a7c0890bcf0a36d1babe25e1cc73b6cfea
KatKat0729 <kat.wightman@ucas-edu.net>
2025-10-08 14:33:41 -0600

    finished expressions practice assignment

---
+++ c_programming_2/practices/expressions_practice.c
@@ -0,0 +1,35 @@
+// KW 7th Expressions Practice
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+    int answer1 = 7-24/8*4+6;
+    printf("Answer 1 is %i\n", answer1);
+
+    int answer2 = 18/3-7+2*5;
+    printf("Answer 2 is %i\n", answer2);
+
+    int answer3 = 6*4/12+72/8-9;
+    printf("Answer 3 is %i\n", answer3);
+
+    int answer4 = (17-6/2)+4*3;
+    printf("Answer 4 is %i\n", answer4);
+
+    int answer5 = -2*(1*4-2/2)+(6+2-3);
+    printf("Answer 5 is %i\n", answer5);
+
+    int answer6 = -1 * ((3-4*7)/5)-2*24/6;
+    printf("Answer 6 is %i\n", answer6);
+
+    int answer7 = ((3*(pow(5,2)))/15)-(5-pow(2,2));
+    printf("Answer 7 is %i\n", answer7);
+
+    int answer8 = ((pow(1,4))*(pow(2,2))+(pow(3,3)))-(pow(2,5))/4;
+    printf("Answer 8 is %i\n", answer8);
+
+    int answer9 = pow((22/2-2*5),2)+pow((4-6/6),2);
+    printf("Answer 9 is %i\n", answer9);
+
+}
