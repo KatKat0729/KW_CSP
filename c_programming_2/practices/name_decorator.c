08d604b41fc01333011a87b9a3aa99cc348d4082
KatKat0729 <kat.wightman@ucas-edu.net>
2025-10-10 01:03:21 +0000

    finished name decorator and silly sentences

---
+++ c_programming_2/practices/name_decorator.c
@@ -0,0 +1,21 @@
+// KW 7th Name Decorator
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+
+    char name[20]; 
+    
+    printf("Hello, I am the name decorator, please tell me your name! -\n");
+    scanf("%s", name);
+
+    printf("~^~ %s ~^~\n", name);
+
+    printf("<<< %s >>>\n", name);
+
+    printf("--- %s ---\n", name);
+
+    printf("_.^._ %s _.^._\n", name);
+
+    return 0;
+}
--- c_programming_2/practices/silly_sentences.c
+++ c_programming_2/practices/silly_sentences.c
@@ -4,28 +4,25 @@
 
 int main(void){
     // Roses are [ADJECTIVE], Violets are [COLOR], Sugar is [NOUN], and so are [PLURAL NOUN]
-    char adjective = 0;
-    char color = 0;
-    char adjective2 = 0;
-    char plural_noun = 0;
+
+    char adjective[25];
+    char color[25];
+    char adjective2[25];
+    char plural_noun[25];
 
     printf("Give me an adjective - \n");
-    
+    scanf("%s", adjective);
 
     printf("Give me a color - \n");
-    
+    scanf("%s", color);
 
     printf("Give me another adjective - \n");
-    
+    scanf("%s", adjective2);
 
     printf("Give me a plural noun - \n");
+    scanf("%s", plural_noun);
     
-    scanf("%s", &adjective);
-    scanf("%s", &color);
-    scanf("%s", &adjective2);
-    scanf("%s", &plural_noun);
-
-    printf("Roses are %s, Violets are %s, Sugar is %s, and so are", adjective, color, adjective2, plural_noun);
+    printf("Roses are %s, Violets are %s, Sugar is %s, and so are %s.", adjective, color, adjective2, plural_noun);
 
     return 0;
 }
