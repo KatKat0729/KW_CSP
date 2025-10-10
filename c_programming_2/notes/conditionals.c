4927cff9296f176de15780eb7ca6bd5c10e94f33
KatKat0729 <kat.wightman@ucas-edu.net>
2025-10-10 14:04:13 -0600

    trying to fix my code

---
+++ c_programming_2/notes/conditionals.c
@@ -0,0 +1,31 @@
+// KW 7th Conditionals Notes
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+
+    int grade;
+
+    printf("What is your grade percent? - \n");
+    scanf("%d",&grade);
+
+    if(grade >= 90){
+        printf("You are passing!\n");
+    }else{
+        printf("You are failing :(\n");
+    }
+    
+
+    //What puts something inside of the “if” statement?
+        // if
+    //How are conditions written in C?
+
+    //How do we write elif conditions in C?
+        // else if
+    //When do else conditions run?
+
+    //What are the 3 logical operators in C?
+
+    return 0;
+}
