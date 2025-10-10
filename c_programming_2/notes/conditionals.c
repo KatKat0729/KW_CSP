83f7a08f1bc00783201537f9996bf7a6418ecf1f
KatKat0729 <kat.wightman@ucas-edu.net>
2025-10-10 14:21:25 -0600

    finished conditionals notes

--- c_programming_2/notes/conditionals.c
+++ c_programming_2/notes/conditionals.c
@@ -2,30 +2,49 @@
 
 #include <stdio.h>
 #include <string.h>
-
+/* Logical operators
+ %% and
+|| or
+! not
+*/
 int main(void){
 
+    /*1.What puts something inside of the “if” statement?
+        - if
+    
+    2. How are conditions written in C? (Below is answer and example)*/
+
     int grade;
+    char name[50];
 
     printf("What is your grade percent? - \n");
     scanf("%d",&grade);
 
-    if(grade >= 90){
-        printf("You are passing!\n");
+    printf("What is your name? - \n");
+    scanf("%s",&name);
+
+    //printf("", strcmp(name, "Ms.Larose"));
+    if(strcmp(name, "Ms.LaRose") == 0){
+        printf("You don't get a grade!\n");
+    }else if(grade >= 90){
+        printf("You have an A!\n");
+    }else if(grade >= 80){
+        printf("You have a B!\n");
+    }else if(grade >= 70){
+        printf("You have a C!\n");
     }else{
-        printf("You are failing :(\n");
+        printf("You are failing\n");
     }
     
-
-    //What puts something inside of the “if” statement?
-        // if
-    //How are conditions written in C?
-
-    //How do we write elif conditions in C?
-        // else if
-    //When do else conditions run?
-
-    //What are the 3 logical operators in C?
-
+    /* 3. How do we write elif conditions in C?
+        - else if
+    4. When do else conditions run?
+        - if none of the above conditions are true, it will print the else condition.
+    5. What are the 3 logical operators in C?
+        - and &&
+        - or ||
+        - not !
+        */
+       
     return 0;
 }
