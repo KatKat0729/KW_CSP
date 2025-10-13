2aaf95053d28469b72f9999a28d7a96b9e030939
KatKat0729 <kat.wightman@ucas-edu.net>
2025-10-13 14:42:09 -0600

    started FizzBuzz assignment

---
+++ c_programming_2/practices/fizzbuzz.c
@@ -0,0 +1,21 @@
+// KW 7th FizzBuzz
+
+#include <stdio.h>
+
+int main(void){
+    char x = 1;
+
+    while(char < 51){
+        if(x % 15 == 0);{
+            printf("FizzBuzz");
+        }
+        else if(x % 5 ==0);{
+            printf("Buzz");
+        }else if(x % 3 ==0);{
+            printf("Fizz")
+        }else
+
+    }
+    }
+    return 0;
+}
