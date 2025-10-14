841f502c5b916148b960662e1440b54a8c40c622
KatKat0729 <kat.wightman@ucas-edu.net>
2025-10-14 14:42:02 -0600

    started updated hello world

---
+++ c_programming_2/practices/update_hello.c
@@ -0,0 +1,15 @@
+// KW 7th Updated Hello World
+void hello(char* name){
+    printf("Hello %s!\n");
+}
+
+#include <stdio.h>
+int main(void){
+    hello("Vinnny");
+    hello("Mars");
+    hello("Ana");
+    hello("Nathan");
+    hello("Adam");
+    
+    return 0;
+}
