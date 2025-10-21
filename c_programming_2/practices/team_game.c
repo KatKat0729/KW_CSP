3e3391e48a2a7c58f2b831a8c49d487b3b3f5a98
KatKat0729 <kat.wightman@ucas-edu.net>
2025-10-21 14:41:58 -0600

    copy and pasted team project

---
+++ c_programming_2/practices/team_game.c
@@ -0,0 +1,77 @@
+// SC AS KW GA 7th C Final, hangman
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+char display_word(letter, display)
+    char* display = " _ ";
+    for (letter in word){
+        if (letter in guessed_letters){
+            printf(letter, end="");
+        else{}
+        printf(" ")
+        }
+    }
+:
+    def did_guess_all_lettirs();:
+        for letter in wordse
+            if True
+
+ not i guessed_letters
+o0129 in t =;;98
+                reloop
+        printf("Congratulations, you won with %d lives left!\n", lives);
+            sys.exit();
+        display_word(){
+            str_guess = printf("Guess a letter - \n").strip().lower();
+        if (len(guess) > 1){
+            printf("Stop wasting my time.");
+        }
+            sys.exit();
+        guessed_letters.append(guess) //append adds something to a list (We are adding the guess to the list of guessed letters);
+        if (guess in word){
+            printf("Correct.");
+        }
+
+        else{
+            lives -= 1
+            printf("Incorrect.");
+        }
+        
+        if did_guess_all_letters();
+            printf("Congratulations, you won with %d lives left!\n", lives);
+            sys.exit();
+        }}tart of game lop
+    while lives(>=1){
+        if()did_guess_all_letters){
+            printf("Congratulations, you won with %d lives left!\n", lives)
+        }
+
+        }
+        
+    }
+    
+
+int main(void){
+    char word_list = {"hello"};
+    srand(time(NULL));
+    char word[][15] = {“Alex”, “Katie”, “Vienna”, “Tia”, “Treyson”, “Xavier”, “Jake”};
+    for(int i=0; i<5; i++){
+    int num = rand() % 8;
+    printf(“Our random person is %s\n”, word[num]);
+int lives = 7;
+    char guessed_letters = [];
+    
+    display_word();
+    did_guess_all_letters();
+    
+printf("You don't have any more guesses, you lose. The word was %d", word);
+sys.exit()
+
+    
+
+
+
+    return 0;
+}
+
